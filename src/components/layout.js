6e5bb082bc8c577e9789897174b54fce2ccc1908
Wojciech Marek <marek.wojciech@outlook.com>
2022-03-13 18:51:21 +0100

    Add missing react icons dep and add Header implemetation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,12 @@
 import * as React from "react"
 
 import Header from "./Header"
+import { GlobalStyle } from "./styles/GlobalStyle"
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
 
       <main>{children}</main>
---
+++ src/data/MenuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+  { title: "About", link: "/about" },
+  { title: "Trips", link: "/trips" },
+  { title: "Carriers", link: "/carriers" },
+  { title: "Contact", link: "/contact" },
+]
